ae9be8e69a803e1cbe112d39e85e4d40d52b3a31
unknown <ivan.soportetec@gmail.com>
2026-02-24 09:18:50 -0600

    remove tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,25 +61,7 @@ export default function RootLayout({
           `}
         </Script>
 
-        {/* Google Tag Manager (deferred) */}
-        <Script id="gtm-script" strategy="lazyOnload">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NFLHXXGK');`}
-        </Script>
-
-        {/* Google Analytics (deferred) */}
-        <Script src="https://www.googletagmanager.com/gtag/js?id=G-W6LV22900R" strategy="lazyOnload" />
-        <Script id="gtag-init" strategy="lazyOnload">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-W6LV22900R');
-          `}
-        </Script>
+       
       </head>
       <body>
         <PreloaderOverlay />
